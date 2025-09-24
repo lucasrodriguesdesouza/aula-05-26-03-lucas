73c3d51c53c2ef970ca09ba1b7f3cff6d89ddeec
lucasrodrigues <lucas.rsouza16@aluno.edu.es.gov.br>
2025-09-24 08:51:00 -0300

    Create index.php

---
+++ Aula 05 3tri/index.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title> Aula 05  PHP </title>
+    </head>
+    <body>
+        <?php
+            $n1= 2;
+            $n2= $_GET["x"];
+
+            echo "Os valores informados são $n1 e $n2 <br>"
+        ?>
+    </body>
+</html>
