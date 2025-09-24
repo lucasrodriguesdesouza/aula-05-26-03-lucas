ab2d0a86c1b35bb61e42e1d2faac656df7d9262a
lucasrodrigues <lucas.rsouza16@aluno.edu.es.gov.br>
2025-09-24 08:57:06 -0300

    Update index.php

--- Aula 05 3tri/index.php
+++ Aula 05 3tri/index.php
@@ -7,7 +7,9 @@
             $n1= 2;
             $n2= $_GET["x"];
 
-            echo "Os valores informados são $n1 e $n2 <br>"
+            echo "Os valores informados são $n1 e $n2 <br>";
+            echo "O valor absoluto de $n1 é",abs($n2);
+            echo "<br> o resultado de $n2  <sup>$n1</sup> é igual a" .pow($n2,$n1);
         ?>
     </body>
 </html>
