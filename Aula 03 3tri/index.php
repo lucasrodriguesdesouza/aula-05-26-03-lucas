79e6b16b6ac8178bc01044d831752c20ded4bd4a
lucasrodrigues <lucas.rsouza16@aluno.edu.es.gov.br>
2025-09-24 08:49:39 -0300

    Create index.php

---
+++ Aula 03 3tri/index.php
@@ -0,0 +1,20 @@
+<html>
+    <head>
+        <title> Aula 03  PHP </title>
+    </head>
+    <body>
+        <?php
+            $nome1="Lucas";
+            $nome2="Lucas";
+            $n1=10;
+            $n2=2;
+            $soma=$n1+$n2;  
+
+            echo"Meu nome é" .$nome1;
+            echo"<br> <br> Meu nome é $nome1 e minha dupla é $nome2";
+          
+            echo"<br> <br> A soma da variavel $n1 com $n2 e igual a $soma";
+            echo"<br> <br> O resultado da subtração de $n1 e $n2 é igual a: " .($n1-$n2) .".";
+        ?>
+    </body>
+</html> 
