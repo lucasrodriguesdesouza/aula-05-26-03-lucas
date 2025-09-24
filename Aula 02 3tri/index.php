fcebb9d5596fa5e478e4807ac741a26c67cebf32
lucasrodrigues <lucas.rsouza16@aluno.edu.es.gov.br>
2025-09-24 08:49:00 -0300

    Create index.php

---
+++ Aula 02 3tri/index.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title> Aula 02 PHP </title>
+    </head>
+    <body>
+        <?php
+            $nome1="Lucas";
+            $nome2="Lucas";
+
+            echo "Meu nome é" .nome1;
+        ?>
+    </body>
+</html>
