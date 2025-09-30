9b7509ceb26eb8822f4935c29cc4965c4f452dd4
lucasrodrigues <lucas.rsouza16@aluno.edu.es.gov.br>
2025-09-30 11:09:16 -0300

    Create index.php

---
+++ Aula 06 3tri/index.php
@@ -0,0 +1,31 @@
+<html>
+    <head>
+        <title> Aula 06  PHP </title>
+    </head>
+    <body>
+        <h3> Operadores de atribuição <h3>
+        <hr>
+        <?php
+            $a=2;
+            $b=$a+5;
+            
+            echo"A é igual a $a e B é igual a $b.";
+            echo"<br><br>";
+            
+            $a=$a + 3;
+            echo"Agora A é igual a $a.";
+            echo"<br><br>";
+
+            $b+=8;
+            echo"Agora A é igual a $b";
+            echo"<br><br>";
+            
+            $b-=5;
+            echo"Agora A é igual a $a.";
+            echo"<br><br>";
+
+
+
+        ?>
+    </body>
+</html>
